1ca00c546702c1cf65c25eb0e31a2c01ee7613e8
SKieronski <GitPW123!@#>
2022-02-09 20:17:00 -0600

    Added process.env.port to index.js for heroku to use on deployment.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
-app.set('port', 8000)
+app.set('port', process.env.PORT || 8000)
 
 
 //==================================
