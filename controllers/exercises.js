c85773c0da34e4a1dd069c4928b3ad73667bd1a9
SKieronski <GitPW123!@#>
2022-02-13 17:13:03 -0600

    Modified PUT requests since they were not working as expected.

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -3,11 +3,8 @@ const router = express.Router()
 const Exercise = require('../models/exercise')
 const Routine = require('../models/routine')
 
-//NOT SURE IF WE NEED THIS ONE
-//GET exercise by ID
+//GET
 router.get('/', (req, res, next) => {
-    const exerciseData = req.body
-    const exerciseId = exerciseData.exerciseId
     Exercise.find()
         .then((exercises) => res.json(exercises))
         .catch(next)
@@ -39,19 +36,20 @@ router.delete('/:id', (req, res, next) => {
 }) 
 
 //Update/patch by id -- don't use PUT because only changing one piece?
-router.patch('/:id', (req, res, next) => {
+router.put('/:id', (req, res, next) => {
     const id = req.params.id
     const exerciseData = req.body
 
-    Routine.findOne({
-        'exercises._id': id
-    })
+    Routine.findOne({ 'exercises._id': id })
         .then((routine) => {
             const exercise = routine.exercises.id(id)
             exercise.set(exerciseData)
             return routine.save()
         })
-        .then(() => res.sendStatus(204))
+        .then((exercise) => {
+            res.sendStatus(200)
+            res.json(exercise)
+        })
         .catch(next)
 })
 
--- controllers/routines.js
+++ controllers/routines.js
@@ -25,13 +25,36 @@ router.post('/', (req, res, next) => {
     .catch(next)
 })
 
+//UPDATE/PUT routine by id
+router.put('/:id', async (req, res, next) => {
+    console.log(req.body);
+    try {
+        const routineToUpdate = await Routine.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+        console.log("in routines put try");
+        res.status(200).json(routineToUpdate);
+    } catch (err) {
+        next(err);
+    }
+})
+
 //UPDATE/PATCH routine by id
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id
-    const routineData = req.body
-    Routine.findByIdAndUpdate({ _id: id }, routineData, { new: true })
-        .then((routine) => res.json(routine))
-        .catch(next)
+router.patch("/:id", async (req, res, next) => {
+    console.log(req.body);
+    try {
+        const routineToUpdate = await Routine.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+        console.log("in routines patch try");
+        res.status(200).json(routineToUpdate);
+    } catch (err) {
+        next(err);
+    }
 })
 
 
